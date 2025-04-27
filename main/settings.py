a55559540c55b9790e53cb5c6c45d546aaba5bad
Callum <callumjones93@hotmail.com>
2025-04-27 23:12:50 +0100

    removed links, pictures, and videos from summernote field

--- main/settings.py
+++ main/settings.py
@@ -88,8 +88,7 @@ SUMMERNOTE_CONFIG = {
             ['color', ['color']],
             ['para', ['ul', 'ol', 'paragraph']],
             ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'codeview', 'help']],
+            ['view', ['fullscreen', 'help']],
         ],
     
 
