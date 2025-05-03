c2cf9a761eb85715ade818eb720b6f2e34b628f9
Callum <callumjones93@hotmail.com>
2025-05-03 20:22:04 +0100

    Add postgres database

--- main/settings.py
+++ main/settings.py
@@ -152,18 +152,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DEFAULT_DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-
-
 DATABASES = {
-    'default': dj_database_url.config(default=DEFAULT_DATABASE_URL)
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
