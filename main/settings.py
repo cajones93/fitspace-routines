7b2c96b16cad64d816e7190ce2a54c12a21b4861
Callum <callumjones93@hotmail.com>
2025-05-04 13:30:42 +0100

    fixed responsiveness issues

--- main/settings.py
+++ main/settings.py
@@ -199,7 +199,7 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
